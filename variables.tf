5d5bdbf512ecd30f8cee8540c97c8a91e7b4e0c3
Dmitriy Solodukha <dspv@me.com>
2024-05-23 13:59:52 +0300

    Initial Commit: 2024-05-23

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable account {}
+
+variable name_prefix {
+  type = string
+}
+variable standard_tags {
+  type = map(string)
+}
+variable ecs_cluster {
+  type = string
+}
+variable cloudwatch_logs_retention_in_days {
+  type    = number
+  default = 3
+}
+
+variable turn_on_schedule {
+  description = "Turn ON Schedule"
+  default     = "cron(0 8 ? * SUN-FRI *)"
+}
+
+variable turn_off_schedule {
+  description = "Turn OFF Schedule"
+  default     = "cron(0 22 ? * SUN-FRI *)"
+}
